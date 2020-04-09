d1bb1391a5ce931580592e9e3d49a823a0bbc1b9
xiaoludan <13651117308@163.com>
2020-04-09 22:18:41 +0800

    eee4

---
+++ app/src/main/java/com/example/diandi/activity/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.diandi.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.diandi.R;
+import com.example.diandi.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    public int initLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/diandi/fragment/PrivateLetterFragment.java
@@ -0,0 +1,21 @@
+package com.example.diandi.fragment;
+
+import com.example.diandi.R;
+import com.example.diandi.base.BaseFragment;
+
+public class PrivateLetterFragment extends BaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_private_letter;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
